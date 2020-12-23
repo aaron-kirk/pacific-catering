d3fe455e415b591e9bffb8be6a8886ec8883be24
littlebro5 <ajkirk1115@gmail.com>
2020-12-22 20:39:39 -0800

    Adjusted menu nav and started menu select functionality
    Menu sticky nav now solid color and changing menu selection now adjusts menu title

--- menus.js
+++ menus.js
@@ -5,24 +5,49 @@ let lunchButton = document.querySelector("#lunch");
 let dinnerButton = document.querySelector("#dinner");
 let corporateButton = document.querySelector("#corporate");
 
+let menuTitle = document.querySelector(".menu-title");
+
+let currentMenu = "appetizer";
+
 function showMenu(menu) {
     removeAllActive();
-
+    currentMenu = menu;
+    console.log(menuTitle);
     switch (menu) {
         case "appetizer":
             appetizerButton.classList.add("active");
+            menuTitle.style.opacity = "0";
+            setTimeout(() => {
+                menuTitle.innerHTML = "Appetizer Menu";
+                menuTitle.style.opacity = "1";
+            }, 250);
             break;
         
         case "lunch":
             lunchButton.classList.add("active");
+            menuTitle.style.opacity = "0";
+            setTimeout(() => {
+                menuTitle.innerHTML = "Lunch Menu";
+                menuTitle.style.opacity = "1";
+            }, 250);
             break;
         
         case "dinner":
             dinnerButton.classList.add("active");
+            menuTitle.style.opacity = "0";
+            setTimeout(() => {
+                menuTitle.innerHTML = "Dinner Menu";
+                menuTitle.style.opacity = "1";
+            }, 250);
             break;
 
         case "corporate":
             corporateButton.classList.add("active");
+            menuTitle.style.opacity = "0";
+            setTimeout(() => {
+                menuTitle.innerHTML = "Corporate Menu";
+                menuTitle.style.opacity = "1";
+            }, 250);
             break;
 
         default:
@@ -58,10 +83,13 @@ document.addEventListener("scroll", (e) => {
 
     if(scrollDistance < 0.05*window.innerHeight) {
         nav.style.top = "0";
+        nav.style.boxShadow = "none";
     } else if(scrollDistance < prevScrollDistance) {
         nav.style.top = "0";
+        nav.style.boxShadow = "0 0 5px 0";
       } else {
         nav.style.top = "-10vh";
+        nav.style.boxShadow = "none";
       }
 
     prevScrollDistance = scrollDistance
