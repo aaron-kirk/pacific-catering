1db7a6a9509f8c216ff238b84838b75c5c88ea6f
Aaron <aaron.awesome.kirk@gmail.com>
2021-02-06 19:15:18 -0800

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Navbar from '../components/Navbar';
+
+class Home extends Component {
+
+    render() {
+        
+    }
+}
+
+export default Home;
